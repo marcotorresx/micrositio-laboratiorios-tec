67848210e1080a7f0f4215d1c35a287c0e822d0f
Marco Torres <marcotorres258@outlook.com>
2022-05-30 15:51:09 -0500

    Add home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import "./Home.sass";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      {/* Info */}
+      <h1>¡Bienvenido al Micrositio de Laboratorios!</h1>
+      <h4>
+        Aquí podrás buscar y ver los videos de los materiales del laboratorio
+        que desees.
+      </h4>
+      <p>Selecciona la categoría que buscas:</p>
+
+      {/* Category */}
+      <div className="category">
+        <select class="form-select">
+          <option value="1">Cat 1</option>
+        </select>
+        <button className="btn btn-outline-primary">Buscar</button>
+      </div>
     </div>
   );
 }
