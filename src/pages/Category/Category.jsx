704476f9dca5653dc1209d6cc5fd3ced7e0122a9
Marco Torres <marcotorres258@outlook.com>
2022-11-05 15:31:32 -0600

    Resources list

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -5,13 +5,13 @@ import { useAppContext } from "context/Context";
 import { getResources, deleteCategory } from "context/actions";
 import { useNavigate } from "react-router-dom";
 import GenericModal from "components/GenericModal/GenericModal";
+import Loader from "components/Loader/Loader";
 
 export default function Category() {
   // Variables
   const {
     categoryOnView: category,
     isAdmin,
-    categoryOnView,
     setCategoryOnView,
     setResourceOnView,
     categories,
@@ -20,8 +20,6 @@ export default function Category() {
   const [loaded, setLoaded] = React.useState(false);
   const [resources, setResources] = React.useState(null);
   const [showDeleteModal, setShowDeleteModal] = React.useState(false);
-
-  // Tools
   const navigate = useNavigate();
 
   // Get resources
@@ -33,8 +31,8 @@ export default function Category() {
 
   // Delete category handler
   function deleteCategoryHandler() {
-    deleteCategory(categoryOnView.id);
-    setCategories(categories.filter((c) => c.id !== categoryOnView.id));
+    deleteCategory(category.id);
+    setCategories(categories.filter((c) => c.id !== category.id));
     setCategoryOnView(null);
     alert("Categoría eliminada exitosamente");
     navigate("/");
@@ -44,56 +42,60 @@ export default function Category() {
     getResourcesHandler();
   });
 
-  return (
-    loaded && (
-      <div className="category">
-        {/* Title */}
-        <h1>{category.category}</h1>
+  return loaded ? (
+    <div className="category">
+      {/* Title */}
+      <h1>{category?.category || "Sin Título"}</h1>
 
-        {isAdmin && (
-          <button
-            className="add_resource_btn btn btn-primary"
-            onClick={() => {
-              setResourceOnView(null);
-              navigate("/private/resource");
-            }}
-          >
-            + Agregar Recurso
-          </button>
-        )}
+      {/* Add Resource */}
+      {isAdmin && (
+        <button
+          className="add_resource_btn btn btn-primary"
+          onClick={() => {
+            setResourceOnView(null);
+            navigate("/private/resource");
+          }}
+        >
+          + Agregar Recurso
+        </button>
+      )}
 
-        {/* Resources */}
-        {resources?.length > 0 ? (
-          <div className="resources">
-            {resources.map((r) => (
-              <Resource resource={r} key={r.id} />
-            ))}
-          </div>
-        ) : (
-          <p className="no_data">No hay recursos en esta categoría</p>
-        )}
+      {/* Resources */}
+      {resources?.length > 0 ? (
+        <ul className="list-group resources_list">
+          {resources.map((r) => (
+            <li className="list-group-item resources_item" key={r.id}>
+              <p className="name">{r?.title || "Sin nombre"}</p>
+              <p className="type">Video</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="no_data">No hay recursos en esta categoría</p>
+      )}
 
-        {/* Delete Btn */}
-        {isAdmin && (
-          <button
-            className="btn btn-danger mt-2"
-            onClick={() => setShowDeleteModal(true)}
-          >
-            Eliminar Categoría
-          </button>
-        )}
+      {/* Delete Btn */}
+      {isAdmin && (
+        <button
+          className="btn btn-danger mt-2"
+          onClick={() => setShowDeleteModal(true)}
+        >
+          Eliminar Categoría
+        </button>
+      )}
 
-        {/* Delete category modal */}
-        {showDeleteModal && (
-          <GenericModal
-            title="Borrar Categoría"
-            onContinue={deleteCategoryHandler}
-            onCancel={() => setShowDeleteModal(false)}
-          >
-            <p>¿Estás seguro que quieres borrar la categoría?</p>
-          </GenericModal>
-        )}
-      </div>
-    )
+      {/* Delete category modal */}
+      {showDeleteModal && (
+        <GenericModal
+          title="Borrar Categoría"
+          onContinue={deleteCategoryHandler}
+          onCancel={() => setShowDeleteModal(false)}
+        >
+          <p>¿Estás seguro que quieres borrar la categoría?</p>
+        </GenericModal>
+      )}
+    </div>
+  ) : (
+    <Loader />
   );
 }
