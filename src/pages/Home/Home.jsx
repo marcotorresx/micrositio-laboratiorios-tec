f3f0b2e8375ec9c231d2d6fbf0d3d342851c6115
Marco Torres <marcotorres258@outlook.com>
2022-11-17 08:09:37 -0600

    Banners

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,15 +7,22 @@ export default function Home() {
   const { categories, setCategoryOnView } = useAppContext();
   const navigate = useNavigate();
 
+  function getRandomBanner() {
+    const staticBanners = [
+      "/imgs/banners/0.jpg",
+      "/imgs/banners/1.jpg",
+      "/imgs/banners/2.jpg",
+      "/imgs/banners/3.jpg",
+      "/imgs/banners/4.png",
+      "/imgs/banners/5.jpg",
+    ];
+    return staticBanners[Math.floor(Math.random() * staticBanners.length)];
+  }
+
   return (
     <div className="home">
-      {/* Info */}
+      {/* TItle */}
       <h1>¡Bienvenido al Micrositio de Laboratorios!</h1>
-      <h4>
-        Aquí podrás buscar y ver los videos de los materiales del laboratorio
-        que desees.
-      </h4>
-      <p className="categories_title">Categorías:</p>
 
       {/* Categories */}
       {categories.length > 0 ? (
@@ -29,13 +36,15 @@ export default function Home() {
                 navigate(`/category`);
               }}
             >
-              <img src="/imgs/folder.png" alt="Folder" className="icon" />
-              <p>{c.category || "Sin nombre"}</p>
+              <div className="item_content">
+                <p>{c.category || "Sin nombre"}</p>
+              </div>
+              <img src={c.banner || getRandomBanner()} alt="Banner" />
             </div>
           ))}
         </div>
       ) : (
-        <p className="no_data">No hay recursos en esta categoría</p>
+        <p className="no_data">No categorías disponibles</p>
       )}
     </div>
   );
